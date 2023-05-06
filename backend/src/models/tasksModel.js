9452c3daa57c4e2649f62b4db8ea697c0f7dcc1d
daianaadepaula <daianaadepaula1@gmail.com>
2023-05-06 10:53:14 -0300

    feat: create route delete

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -15,6 +15,11 @@ const createTask = async (task) =>{
   return {insertId: createdTask.insertId};
 };
 
+const deleteTask = async (id) =>{
+  const removedTask = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
+  return removedTask;
+};
+
 module.exports = {
-  getAll, createTask,
+  getAll, createTask, deleteTask,
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -5,6 +5,7 @@ const tasksMiddleware = require('./middlewares/tasksMiddleware');
 const router = express.Router();
 
 router.get('/tasks', tasksController.getAll);
-router.post('/tasks',tasksMiddleware.validateBody, tasksController.createTask);
+router.post('/tasks', tasksMiddleware.validateBody, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
 
 module.exports = router;
